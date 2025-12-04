6392d7cb24219479b579f19f38b3773e0f3d04d5
anands982 <anands9824@gmail.com>
2025-12-04 12:59:12 +0530

    Initial commit

---
+++ src/main/java/com/github/devops/poc/DevopsPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.devops.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devops/poc/DevopsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devops.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
